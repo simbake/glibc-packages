c097c1c720686704ccfabdea349c0c1a431a4aab
Oscar Simba <simbake2009@gmail.com>
2024-12-31 00:06:58 +0300

    Update build.sh

--- gpkg/mesa-vulkan-icd-wrapper-glibc/build.sh
+++ gpkg/mesa-vulkan-icd-wrapper-glibc/build.sh
@@ -6,14 +6,14 @@ TERMUX_PKG_MAINTAINER="xMeM <haooy@outlook.com>"
 TERMUX_PKG_VERSION="24.3.1"
 TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=git+https://github.com/simbake/mesa
-TERMUX_PKG_GIT_BRANCH=wrapper
+TERMUX_PKG_GIT_BRANCH=master
 _COMMIT=578ceaf21b23233f90d1b31add7a0dfdfb31b860
 #TERMUX_PKG_DEPENDS="vulkan-headers-glibc, libwayland-protocols-glibc, libglvnd-glibc, gcc-libs-glibc, libdrm-glibc, libllvm-glibc, libexpat-glibc, zlib-glibc, zstd-glibc, libx11-glibc, libxcb-glibc, libxext-glibc, libxfixes-glibc, libxshmfence-glibc, libxxf86vm-glibc, libwayland-glibc, libvdpau-glibc, libomxil-bellagio-glibc, libva-glibc, libxml2-glibc, libelf-glibc, libbz2-glibc, libclc-glibc"
 TERMUX_PKG_DEPENDS="vulkan-headers-glibc, vulkan-icd-loader-glibc, libdrm-glibc, libx11-glibc, libxcb-glibc, libxshmfence-glibc, libwayland-glibc, zlib-glibc, zstd-glibc"
 TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols-glibc, xorgproto-glibc, libxrandr-glibc"
 TERMUX_PKG_PYTHON_COMMON_DEPS="mako, setuptools, pyyaml"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--Dandroid-libbacktrace=enabled
+-Dandroid-libbacktrace=disabled
 -Db_ndebug=true
 -Dgbm=disabled
 -Dopengl=false
@@ -26,7 +26,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dlibunwind=disabled
 -Dmicrosoft-clc=disabled
 -Dvalgrind=disabled
--Dandroid-stub=true
+-Dandroid-stub=false
 "
 termux_step_pre_configure() {
 	export MESON_PACKAGE_CACHE_DIR="${TERMUX_PKG_SRCDIR}"
