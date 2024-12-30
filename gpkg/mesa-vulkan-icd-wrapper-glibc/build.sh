314f3e915b325cf8da9953f108b44b0e7d403c98
Oscar Simba <simbake2009@gmail.com>
2024-12-30 22:06:46 +0300

    Update build.sh

--- gpkg/mesa-vulkan-icd-wrapper-glibc/build.sh
+++ gpkg/mesa-vulkan-icd-wrapper-glibc/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_LICENSE_FILE="docs/license.rst"
 TERMUX_PKG_MAINTAINER="xMeM <haooy@outlook.com>"
 TERMUX_PKG_VERSION="24.3.1"
 TERMUX_PKG_REVISION=4
-TERMUX_PKG_SRCURL=git+https://github.com/xMeM/mesa
+TERMUX_PKG_SRCURL=git+https://github.com/simbake/mesa
 TERMUX_PKG_GIT_BRANCH=wrapper
 _COMMIT=578ceaf21b23233f90d1b31add7a0dfdfb31b860
 #TERMUX_PKG_DEPENDS="vulkan-headers-glibc, libwayland-protocols-glibc, libglvnd-glibc, gcc-libs-glibc, libdrm-glibc, libllvm-glibc, libexpat-glibc, zlib-glibc, zstd-glibc, libx11-glibc, libxcb-glibc, libxext-glibc, libxfixes-glibc, libxshmfence-glibc, libxxf86vm-glibc, libwayland-glibc, libvdpau-glibc, libomxil-bellagio-glibc, libva-glibc, libxml2-glibc, libelf-glibc, libbz2-glibc, libclc-glibc"
@@ -26,7 +26,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dlibunwind=disabled
 -Dmicrosoft-clc=disabled
 -Dvalgrind=disabled
--Dandroid-stub=true
+-Dandroid-stub=false
 "
 termux_step_pre_configure() {
 	export MESON_PACKAGE_CACHE_DIR="${TERMUX_PKG_SRCDIR}"
