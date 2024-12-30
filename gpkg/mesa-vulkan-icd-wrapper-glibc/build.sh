470fcc0c950aad0a4b88daeb152a7832d580d1a6
Oscar Simba <simbake2009@gmail.com>
2024-12-30 18:39:07 +0300

    Update build.sh

--- gpkg/mesa-vulkan-icd-wrapper-glibc/build.sh
+++ gpkg/mesa-vulkan-icd-wrapper-glibc/build.sh
@@ -10,7 +10,6 @@ TERMUX_PKG_GIT_BRANCH=wrapper
 _COMMIT=578ceaf21b23233f90d1b31add7a0dfdfb31b860
 TERMUX_PKG_DEPENDS="vulkan-headers-glibc, libwayland-protocols-glibc, libglvnd-glibc, gcc-libs-glibc, libdrm-glibc, libllvm-glibc, libexpat-glibc, zlib-glibc, zstd-glibc, libx11-glibc, libxcb-glibc, libxext-glibc, libxfixes-glibc, libxshmfence-glibc, libxxf86vm-glibc, libwayland-glibc, libvdpau-glibc, libomxil-bellagio-glibc, libva-glibc, libxml2-glibc, libelf-glibc, libbz2-glibc, libclc-glibc"
 TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols-glibc, xorgproto-glibc, libxrandr-glibc"
-TERMUX_ON_DEVICE_BUILD=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dcpp_rtti=false
 -Dgbm=disabled
@@ -32,13 +31,11 @@ termux_step_pre_configure() {
 	termux_setup_cmake
 	_WRAPPER_BIN=$TERMUX_PKG_BUILDDIR/_wrapper/bin
 	mkdir -p $_WRAPPER_BIN
-	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
-		sed 's|@CMAKE@|'"$(command -v cmake)"'|g' \
-			$TERMUX_PKG_BUILDER_DIR/cmake-wrapper.in \
-			> $_WRAPPER_BIN/cmake
-		chmod 0700 $_WRAPPER_BIN/cmake
-		termux_setup_wayland_cross_pkg_config_wrapper
-	fi
+	sed 's|@CMAKE@|'"$(command -v cmake)"'|g' \
+	$TERMUX_PKG_BUILDER_DIR/cmake-wrapper.in \
+	> $_WRAPPER_BIN/cmake
+	chmod 0700 $_WRAPPER_BIN/cmake
+	termux_setup_wayland_cross_pkg_config_wrapper
 	export PATH=$_WRAPPER_BIN:$PATH
 }
 
