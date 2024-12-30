59bb7db7f4e4ca343ab571fd956b6e1a419e446b
Oscar Simba <simbake2009@gmail.com>
2024-12-30 19:38:08 +0300

    Update build.sh

--- gpkg/libwayland-protocols/build.sh
+++ gpkg/libwayland-protocols/build.sh
@@ -1,10 +1,11 @@
-TERMUX_PKG_HOMEPAGE=https://wayland.freedesktop.org/
+8TERMUX_PKG_HOMEPAGE=https://wayland.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="Wayland protocols library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
 TERMUX_PKG_VERSION=1.38
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/${TERMUX_PKG_VERSION}/downloads/wayland-protocols-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=ff17292c05159d2b20ce6cacfe42d7e31a28198fa1429a769b03af7c38581dbe
+TERMUX_PKG_BUILD_DEPENDS="libwayland-glibc"
 TERMUX_PKG_DEPENDS="glibc"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
