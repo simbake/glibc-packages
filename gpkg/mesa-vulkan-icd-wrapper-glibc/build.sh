e89b871394c001766ca629fd8041fcb58464a63a
Oscar Simba <simbake2009@gmail.com>
2024-12-30 18:19:42 +0300

    Update build.sh

--- gpkg/mesa-vulkan-icd-wrapper-glibc/build.sh
+++ gpkg/mesa-vulkan-icd-wrapper-glibc/build.sh
@@ -5,12 +5,12 @@ TERMUX_PKG_LICENSE_FILE="docs/license.rst"
 TERMUX_PKG_MAINTAINER="xMeM <haooy@outlook.com>"
 TERMUX_PKG_VERSION="24.3.1"
 TERMUX_PKG_REVISION=4
-TERMUX_PKG_SRCURL=git+https://github.com/simbake/mesa
+TERMUX_PKG_SRCURL=git+https://github.com/xMeM/mesa
 TERMUX_PKG_GIT_BRANCH=wrapper
 _COMMIT=578ceaf21b23233f90d1b31add7a0dfdfb31b860
-TERMUX_PKG_DEPENDS="vulkan-headers-glibc, libwayland-protocols-glibc, libglvnd-glibc, gcc-libs-glibc, libdrm-glibc, libllvm-glibc, libexpat-glibc, zlib-glibc, zstd-glibc, libx11-glibc, libxcb-glibc, libxext-glibc, libxfixes-glibc, libxshmfence-glibc, libxxf86vm-glibc, libwayland-glibc, libvdpau-glibc, libomxil-bellagio-glibc, libva-glibc, libxml2-glibc, libelf-glibc, libbz2-glibc, libclc-glibc"
-TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols-glibc, xorgproto-glibc, libxrandr-glibc, glslang-glibc"
-
+TERMUX_PKG_DEPENDS="vulkan-headers-glibc, libc++ libwayland-protocols-glibc, libglvnd-glibc, gcc-libs-glibc, libdrm-glibc, libllvm-glibc, libexpat-glibc, zlib-glibc, zstd-glibc, libx11-glibc, libxcb-glibc, libxext-glibc, libxfixes-glibc, libxshmfence-glibc, libxxf86vm-glibc, libwayland-glibc, libvdpau-glibc, libomxil-bellagio-glibc, libva-glibc, libxml2-glibc, libelf-glibc, libbz2-glibc, libclc-glibc"
+TERMUX_PKG_BUILD_DEPENDS="libwayland-protocols-glibc, xorgproto-glibc, libxrandr-glibc"
+TERMUX_ON_DEVICE_BUILD=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dcpp_rtti=false
 -Dgbm=disabled
