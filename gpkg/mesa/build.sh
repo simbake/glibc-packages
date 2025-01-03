b6e9ccc917b584b712ccf041f8ab7f81d70d84e5
Oscar Simba <simbake2009@gmail.com>
2025-01-03 08:11:04 +0300

    Update build.sh

--- gpkg/mesa/build.sh
+++ gpkg/mesa/build.sh
@@ -6,36 +6,18 @@ TERMUX_PKG_MAINTAINER="@termux-pacman"
 TERMUX_PKG_VERSION="24.2.6"
 TERMUX_PKG_SRCURL=https://mesa.freedesktop.org/archive/mesa-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=2b68c4a6f204c1999815a457299f81c41ba7bf48c4674b0b2d1d8864f41f3709
-TERMUX_PKG_DEPENDS="vulkan-headers-glibc, libwayland-protocols-glibc, libglvnd-glibc, gcc-libs-glibc, libdrm-glibc, libllvm-glibc, libexpat-glibc, zlib-glibc, zstd-glibc, libx11-glibc, libxcb-glibc, libxext-glibc, libxfixes-glibc, libxshmfence-glibc, libxxf86vm-glibc, libwayland-glibc, libvdpau-glibc, libomxil-bellagio-glibc, libva-glibc, libxml2-glibc, libelf-glibc, libbz2-glibc, libclc-glibc"
+TERMUX_PKG_DEPENDS="vulkan-icd-loader-glibc, libwayland-protocols-glibc, libglvnd-glibc, gcc-libs-glibc, libdrm-glibc, libllvm-glibc, libexpat-glibc, zlib-glibc, zstd-glibc, libx11-glibc, libxcb-glibc, libxext-glibc, libxfixes-glibc, libxshmfence-glibc, libxxf86vm-glibc, libwayland-glibc, libvdpau-glibc, libomxil-bellagio-glibc, libva-glibc, libxml2-glibc, libelf-glibc, libbz2-glibc, libclc-glibc"
 TERMUX_PKG_SUGGESTS="mesa-dev-glibc"
-TERMUX_PKG_BUILD_DEPENDS="llvm-glibc, libwayland-protocols-glibc, xorgproto-glibc, glslang-glibc"
+TERMUX_PKG_BUILD_DEPENDS="llvm-glibc, libwayland-protocols-glibc, xorgproto-glibc, glslang-glibc, vulkan-headers-glibc"
 TERMUX_PKG_PYTHON_COMMON_DEPS="mako, setuptools, pyyaml"
 # disabling libunwind, microsoft-clc and valgrind will improve performance
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--D android-libbacktrace=disabled
--D b_ndebug=true
--D dri3=enabled
--D egl=enabled
--D gallium-drivers=zink,swrast
--D gallium-extra-hud=true
--D gallium-nine=false
--D gallium-va=disabled
--D gallium-vdpau=disabled
--D gallium-xa=disabled
--D gbm=enabled
--D gles1=enabled
--D gles2=enabled
--D glvnd=true
--D glx=dri
--D libunwind=disabled
--D llvm=enabled
--D microsoft-clc=disabled
--D osmesa=true
--D platforms=x11,wayland
--D shared-glapi=enabled
--D valgrind=disabled
--D vulkan-layers=device-select,overlay
+-Dgallium-drivers=zink \
+-Dvulkan-drivers= \
+-Ddri-drivers= \
+-Dplatforms=x11,wayland \
+-Dbuildtype=release
 "
 
 termux_step_pre_configure() {
