f1c29fdec0955fbabc782bcd4bc3cbe4c6f8eb5e
Oscar Simba <simbake2009@gmail.com>
2025-01-03 10:17:28 +0300

    Update build.sh

--- gpkg/mesa/build.sh
+++ gpkg/mesa/build.sh
@@ -6,27 +6,25 @@ TERMUX_PKG_MAINTAINER="@termux-pacman"
 TERMUX_PKG_VERSION="24.2.6"
 TERMUX_PKG_SRCURL=https://mesa.freedesktop.org/archive/mesa-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=2b68c4a6f204c1999815a457299f81c41ba7bf48c4674b0b2d1d8864f41f3709
-TERMUX_PKG_DEPENDS="vulkan-icd-loader-glibc, libwayland-protocols-glibc, libglvnd-glibc, gcc-libs-glibc, libdrm-glibc, libllvm-glibc, libexpat-glibc, zlib-glibc, zstd-glibc, libx11-glibc, libxcb-glibc, libxext-glibc, libxfixes-glibc, libxshmfence-glibc, libxxf86vm-glibc, libwayland-glibc, libvdpau-glibc, libomxil-bellagio-glibc, libva-glibc, libxml2-glibc, libelf-glibc, libbz2-glibc, libclc-glibc"
+TERMUX_PKG_DEPENDS="vulkan-icd-loader-glibc, vulkan-headers-glibc, libwayland-protocols-glibc, libglvnd-glibc, gcc-libs-glibc, libdrm-glibc, libllvm-glibc, libexpat-glibc, zlib-glibc, zstd-glibc, libx11-glibc, libxcb-glibc, libxext-glibc, libxfixes-glibc, libxshmfence-glibc, libxxf86vm-glibc, libwayland-glibc, libvdpau-glibc, libomxil-bellagio-glibc, libva-glibc, libxml2-glibc, libelf-glibc, libbz2-glibc, libclc-glibc"
 TERMUX_PKG_SUGGESTS="mesa-dev-glibc"
-TERMUX_PKG_BUILD_DEPENDS="llvm-glibc, libwayland-protocols-glibc, xorgproto-glibc, glslang-glibc, vulkan-headers-glibc"
+TERMUX_PKG_BUILD_DEPENDS="llvm-glibc, libwayland-protocols-glibc, xorgproto-glibc, glslang-glibc, vulkan-headers-glibc, vulkan-icd-loader-glibc"
 TERMUX_PKG_PYTHON_COMMON_DEPS="mako, setuptools, pyyaml"
 # disabling libunwind, microsoft-clc and valgrind will improve performance
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--Dgbm=disabled \
--Dopengl=true \
--Dllvm=disabled \
--Dshared-llvm=disabled \
--Dplatforms=x11,wayland \
--Dgallium-drivers=virgl,zink \
+-Dcpp_rtti=false
+-Dgbm=disabled
+-Dopengl=false
+-Dllvm=disabled
+-Dshared-llvm=disabled
+-Dplatforms=x11,wayland
+-Dgallium-drivers=zink
 -Dxmlconfig=disabled
+-Dvulkan-drivers=virtio
 "
 
 termux_step_pre_configure() {
-	case $TERMUX_ARCH in
-		arm|aarch64) TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvulkan-drivers=virtio";;
-		*) TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvulkan-drivers=virtio";;
-	esac
 	export MESON_PACKAGE_CACHE_DIR="${TERMUX_PKG_SRCDIR}"
 	export LLVM_CONFIG=$TERMUX_PREFIX/bin/llvm-config
 
